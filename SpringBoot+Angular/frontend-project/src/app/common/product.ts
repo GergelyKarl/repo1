3877b259fab5a050b69b408e6ca1cd4846aee1fb
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2020-03-15 00:47:29 +0100

    ng

---
+++ SpringBoot+Angular/frontend-project/src/app/common/product.ts
@@ -0,0 +1,13 @@
+export class Product {
+
+sku: string;
+name: string;
+description: string;
+unitPrice: number;
+imgUrl:string;
+active:boolean;
+unitsInStock:number;
+dateCreated:Date;
+lastUpdated: Date;
+
+}
---
+++ SpringBoot+Angular/frontend-project/src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/product.service';
+import { Product } from 'src/app/common/product';
+
+@Component({
+  selector: 'app-product-list',
+  //templateUrl: './product-list.component.html',
+  templateUrl: './product-list-table.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products:Product[];
+  constructor(private productService:ProductService) { }
+
+  ngOnInit() {
+  this.listProduct();
+  
+  }
+  listProduct() {
+    this.productService.getProductList().subscribe(data=>{this.products=data;})
+  }
+
+
+
+}
