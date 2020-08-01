a48035fb465d0d9848f55e9742fcfc28b77941a4
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2020-08-01 20:46:48 +0200

    md

---
+++ movie-datebase/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import "./App.css";
+import Movies from "./components/movies";
+
+class App extends Component {
+  state = {};
+  render() {
+    return(<React.Fragment>
+     <main className="container">
+       <Movies />
+     </main>
+     </React.Fragment>)
+  }
+}
+
+export default App;
